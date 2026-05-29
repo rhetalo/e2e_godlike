9877375232b1a971b279c2ab59d5f1278625f7ac
r.hetalo <r.hetalo@godlike.host>
2026-05-29 11:20:50 +0300

    fix config

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,24 +24,37 @@ export default defineConfig({
 
   reporter: 'html',
 
-  use: {
-    baseURL: 'https://godlike.host',
+  // use: {
+  //   baseURL: 'https://godlike.host',
 
-    actionTimeout: 15_000,
-    navigationTimeout: 60_000,
+  //   actionTimeout: 15_000,
+  //   navigationTimeout: 60_000,
 
-    // Трассировка при первом retry — для отладки упавших тестов
-    trace: 'on-first-retry',
+  //   // Трассировка при первом retry — для отладки упавших тестов
+  //   trace: 'on-first-retry',
 
-    viewport: { width: 1800, height: 900 },
-    deviceScaleFactor: 1,
-  },
+  //   viewport: { width: 1800, height: 900 },
+  //   deviceScaleFactor: 1,
+  // },
 
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+    name: 'chromium',
+
+    use: {
+      ...devices['Desktop Chrome'],
+
+      baseURL: 'https://godlike.host',
+
+      actionTimeout: 15_000,
+      navigationTimeout: 60_000,
+
+      trace: 'on-first-retry',
+
+      viewport: { width: 1800, height: 900 },
+      deviceScaleFactor: 1,
     },
+  },
     // {
     //   name: 'firefox',
     //   use: { ...devices['Desktop Firefox'] },
